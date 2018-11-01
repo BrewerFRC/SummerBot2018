da0f35cb1d1d938133ef3c3282d3a7c133aca3d0
BrewerFRC <stevejacobs@iname.com>
2018-11-01 18:34:41 -0400

    Code formatting change

--- src/main/java/org/usfirst/frc/team4564/robot/Bat.java
+++ src/main/java/org/usfirst/frc/team4564/robot/Bat.java
@@ -3,31 +3,29 @@ package org.usfirst.frc.team4564.robot;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 /**
- * Ultrasonic control class
- * Designed for a Y cable on a DIO port to control three ultrasonic sensors.
- * Can also just have them always on.
+ * Ultrasonic control class Designed for a Y cable on a DIO port to control
+ * three ultrasonic sensors. Can also just have them always on.
  * 
  * Created January 2018
  * 
- *  @author Brewer FIRST Robotics Team 4564 
- *  @author Brent Roberts
-*/
+ * @author Brewer FIRST Robotics Team 4564
+ * @author Brent Roberts
+ */
 public class Bat {
-	int cycleDelay = Constants.REFRESH_RATE/10;
+	int cycleDelay = Constants.REFRESH_RATE / 10;
 	long cycleTime = 0;
 	int timePerPulse = 50;
-	
-	//AnalogInput sonic = new AnalogInput(Constants.SONIC_PIN);
-	
-	//Constants
-	//Stolen from 2017
+
+	// AnalogInput sonic = new AnalogInput(Constants.SONIC_PIN);
+
+	// Constants
+	// Stolen from 2017
 	private static final double CORRECTION = 1 / 1.04;
 	private static final double VOLTS_PER_INCH = 5.0 / 1024 * 2.54 * CORRECTION; // Volts per inch constant
-	
+
 	public double getDistance() {
 		return 0f;
-		//return sonic.getVoltage()/VOLTS_PER_INCH;
+		// return sonic.getVoltage()/VOLTS_PER_INCH;
 	}
-	
-	
+
 }
